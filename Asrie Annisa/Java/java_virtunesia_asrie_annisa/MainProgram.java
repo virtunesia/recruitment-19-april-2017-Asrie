e5dfc4603ac81b6d5d021938576cf6a3991c9ab5
asrieannisa <pratiwiasrieannisa@gmail.com>
2017-04-26 05:45:52 +0700

    Update MainProgram.java

--- Asrie Annisa/Java/java_virtunesia_asrie_annisa/MainProgram.java
+++ Asrie Annisa/Java/java_virtunesia_asrie_annisa/MainProgram.java
@@ -4,6 +4,7 @@ public class MainProgram {
 public static void main(String[] args) {
 		
 		Pemesanan p= new Pemesanan("P002", "25/04/2017");
+		Pembayaran p2=new Pembayaran("PB002", 300000);
 		
 	}
 
